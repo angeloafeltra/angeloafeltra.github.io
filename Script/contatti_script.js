8b5f55d14eaf2b3868c3dfd342f6620f1fcdb633
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-11-26 00:03:08 +0100

    send mail

--- Script/contatti_script.js
+++ Script/contatti_script.js
@@ -1,5 +1,8 @@
 const containerMap=document.querySelector('.map_container')
 const containerPreventivo=document.querySelector('.form_container')
+const btn_chiama=document.querySelector('#chiama')
+
+let check_click=false;
 
 function setSizeContainerShowRoom(){
     if(window.innerWidth>=600) {
@@ -8,3 +11,89 @@ function setSizeContainerShowRoom(){
         containerMap.style.height = "400px";
     }
 }
+
+btn_chiama.addEventListener('click', (e)=>{
+    if(!check_click) {
+        document.querySelector('.btn_chiama p').innerHTML = "3394487295";
+        check_click = true;
+    }else{
+        document.querySelector('.btn_chiama p').innerHTML = "CHIAMA ORA";
+        check_click = false;
+    }
+})
+
+
+function Email() {
+    var email = document.modulo.email.value;
+    var oggetto = document.modulo.oggetto.value;
+    var messaggio = document.modulo.messaggio.value;
+    if ((email.indexOf("@") == (-1)) || (email == "") || (email == "undefined")) {
+        alert("Inserire un indirizzo email valido.");
+        document.modulo.email.focus();
+    }
+    else if ((oggetto == "") || (oggetto == "undefined")) {
+        alert("Inserire un oggetto.");
+        document.modulo.oggetto.focus();
+    }
+    else if ((messaggio == "") || (messaggio == "undefined")) {
+        alert("Inserire un messaggio.");
+        document.modulo.messaggio.focus();
+    }
+    else {
+        location.href = "mailto:" + email + "?Subject=" + oggetto + "&Body=" + messaggio;
+    }
+}
+
+function sendMail(){
+    const error=document.querySelector('#errore');
+    //Controllo i valori dei campi nome,mail,telefono e testo
+    var nome = document.modulo.nome.value;
+    var mail= document.modulo.mail.value;
+    var telefono= document.modulo.telefono.value;
+    var msg=document.modulo.messaggio.value;
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var telefonoformat= /^((\+39)|(0039)|(\(00\+39\)))?([ ]?[3][0-9]{2}[ ]?[-\s\.]?\d{6,7})$/;
+
+    if (nome.length==0 || mail.length==0 || telefono.length==0 || msg.length==0){
+        error.innerHTML="Compilare tutti i campi";
+        error.style.display="block";
+        return false;
+    }
+
+    if(!mail.match(mailformat)){
+        error.innerHTML="Inserire una mail valida";
+        error.style.display="block";
+        return false;
+    }
+
+    if(!telefono.match(telefonoformat)){
+        error.innerHTML="Inserire un numero di telefono valido";
+        error.style.display="block";
+        return false;
+    }
+
+    if(!document.getElementById("checkbox").checked){
+        error.innerHTML="Acconsentire il trattamento dei dati";
+        error.style.display="block";
+        return false;
+    }
+
+    error.style.display="none";
+    document.modulo.nome.value="";
+    document.modulo.mail.value="";
+    document.modulo.telefono.value="";
+    document.modulo.messaggio.value="";
+    document.getElementById("checkbox").checked=false;
+
+
+
+    body="Nome:" + nome +" - Mail:" + mail + " - Telefono:" + telefono + " - Messaggio:" + msg;
+    location.href = "mailto:angelo.afeltra99@gmail.com?Subject=Richiesta Preventivo" + "&Body=" + body;
+    return true;
+
+}
+
+var form = document.querySelector('form')
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
--- Script/prodotti_script.js
+++ Script/prodotti_script.js
@@ -1,6 +1,13 @@
 const containerMap=document.querySelector('.map_container')
 const containerPreventivo=document.querySelector('.form_container')
 
+const containerImgProdotto=document.querySelector('.container_main_img')
+const mainImg=document.querySelector('#main_img')
+const subImg1=document.querySelector('#sub_img1')
+const subImg2=document.querySelector('#sub_img2')
+const subImg3=document.querySelector('#sub_img3')
+const subImg4=document.querySelector('#sub_img4')
+
 function setSizeContainerShowRoom(){
     if(window.innerWidth>=600) {
         containerMap.style.height = containerPreventivo.offsetHeight + "px";
@@ -8,3 +15,39 @@ function setSizeContainerShowRoom(){
         containerMap.style.height = "400px";
     }
 }
+
+subImg1.addEventListener('click', (e)=>{
+    main_img=mainImg.src;
+    sub_img=subImg1.src;
+    mainImg.src=sub_img
+    subImg1.src=main_img;
+    //console.log("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_img+") no-repeat center center;")
+    //containerImgProdotto.style.background= "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_Img+") no-repeat center center;";
+})
+
+subImg2.addEventListener('click', (e)=>{
+    main_img=mainImg.src;
+    sub_img=subImg2.src;
+    mainImg.src=sub_img
+    subImg2.src=main_img;
+    //console.log("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_img+") no-repeat center center;")
+    //containerImgProdotto.style.background= "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_Img+") no-repeat center center;";
+})
+
+subImg3.addEventListener('click', (e)=>{
+    main_img=mainImg.src;
+    sub_img=subImg3.src;
+    mainImg.src=sub_img
+    subImg3.src=main_img;
+    //console.log("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_img+") no-repeat center center;")
+    //containerImgProdotto.style.background= "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_Img+") no-repeat center center;";
+})
+
+subImg4.addEventListener('click', (e)=>{
+    main_img=mainImg.src;
+    sub_img=subImg4.src;
+    mainImg.src=sub_img
+    subImg4.src=main_img;
+    //console.log("linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_img+") no-repeat center center;")
+    //containerImgProdotto.style.background= "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+sub_Img+") no-repeat center center;";
+})
