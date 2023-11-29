b6deabee2eb5f4cd2da629aaa75932de9b987bb7
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-11-29 15:34:48 +0100

    update

--- Script/prodotti_script.js
+++ Script/prodotti_script.js
@@ -3,6 +3,7 @@
     const containerMap=document.querySelector('.map_container')
     const containerPreventivo=document.querySelector('.form_container')
     const container_immagini=document.querySelector('.container_immagini')
+    const container_descrizione_prodotto=document.querySelector('.container_descrizione_prodotto')
     //Inserire qui nuovi prodotti, nuove immagini e modificare prodotti e immagini gia esistenti
     const categoriaProdotti=[
         {
@@ -124,7 +125,6 @@
                         'Dotata di maniglie in acciaio inossidabile di alta qualità, questa porta è facile da installare e offre una solida costruzione che garantisce durata nel tempo. ' +
                         'Le dimensioni standard la rendono adatta a una varietà di spazi, mentre il colore neutro si abbina facilmente a qualsiasi arredamento.',
                     list_img: [
-                        'Immagini/Prodotti/Porte/porta1.jpeg',
                         'Immagini/Prodotti/Porte/porta2.jpeg',
                         'Immagini/Prodotti/Porte/porta3.jpeg',
                         'Immagini/Prodotti/Porte/porta4.jpeg',
@@ -142,7 +142,6 @@
                         'Immagini/Prodotti/Porte/porta2.jpeg',
                         'Immagini/Prodotti/Porte/porta3.jpeg',
                         'Immagini/Prodotti/Porte/porta4.jpeg',
-                        'Immagini/Prodotti/Porte/porta5.jpeg',
                     ]
                 },
                 {
@@ -334,10 +333,12 @@
             ]
         }
     ]
-    var prodotto=""
-    var descrizione=""
-    var list_img_prodotto=[]
-    var index_current_img=0;
+
+    let list_sub_category=[]
+    let index_sub_category=0
+    let index_current_img=0;
+
+
 
 /* ... */
 
@@ -354,53 +355,80 @@
         }
     }
 
+    function setSubCategory(element) {
+        let categoria = element.childNodes[1].innerHTML
+
+        for (var i = 0; i < categoriaProdotti.length; i++) {
+            if (categoriaProdotti[i].main_category === categoria) {
+                list_sub_category = categoriaProdotti[i].sub_category
+                index_sub_category = 0
+            }
+        }
+    }
+
+    function generateItemCatalogo(categoriaProdotto){
+        let item_catalogo=document.createElement('div')
+        item_catalogo.className='item'
+
+        let icon_container=document.createElement('div')
+        let icon=document.createElement('img')
+        icon.src="./Immagini/SVG/item.svg"
+        icon.alt="item_symbol"
+        icon.style.width="100%"
+        icon.style.height="100%"
+        icon_container.appendChild(icon);
+
+        let categoria=document.createElement('h2')
+        categoria.innerHTML=categoriaProdotto
+
+        item_catalogo.appendChild(icon_container)
+        item_catalogo.appendChild(categoria);
+
+        item_catalogo.addEventListener('click', function(event) { //Assegno un eventListener all'item
+            setSubCategory(event.currentTarget)
+            showProdotto(0);
+        });
+
+        return item_catalogo;
+    }
+
+    //Funzione per la gestione del catalogo dinamicamente
     function generateDynamicCatalogue(){
         let catalogo=document.querySelector('.container_categorie')
         for(var i=0;i<categoriaProdotti.length;i++){
-            let item_catalogo=document.createElement('div')
-            item_catalogo.className='item'
-
-            let icon_container=document.createElement('div')
-            let icon=document.createElement('img')
-            icon.src="./Immagini/SVG/item.svg"
-            icon.alt="item_symbol"
-            icon.style.width="100%"
-            icon.style.height="100%"
-            icon_container.appendChild(icon);
-
-            let categoria=document.createElement('h2')
-            categoria.innerHTML=categoriaProdotti[i].main_category
-
-            item_catalogo.appendChild(icon_container)
-            item_catalogo.appendChild(categoria);
-
-            catalogo.appendChild(item_catalogo);
-            item_catalogo.addEventListener('click', function(event) {
-                showProdotto(event.currentTarget);
-            });
+            item_catalogo=generateItemCatalogo(categoriaProdotti[i].main_category) //Genero l'item
+            catalogo.appendChild(item_catalogo); //Aggiungo l'item al catalgo
         }
 
-        catalogo.childNodes[1].click();
+        if(catalogo.childNodes.length>0)
+            catalogo.childNodes[1].click(); //Simulo il click sul primo item del catalogo
     }
 
-    function showProdotto(element){
+    //Funzione che mostra il primo sotto-prodotto del prodotto scelto
+    function showProdotto(index) {
 
-        var categoria=element.childNodes[1].innerHTML
+        let prodotto = list_sub_category[index].nome
+        let descrizione = list_sub_category[index].descrizione
+        let list_img_prodotto = list_sub_category[index].list_img
+        index_current_img=0;
 
-        for(var i=0;i<categoriaProdotti.length;i++){
-            if(categoriaProdotti[i].main_category===categoria){
-                prodotto=categoriaProdotti[i].sub_category[0].nome
-                descrizione=categoriaProdotti[i].sub_category[0].descrizione
-                if(categoriaProdotti[i].sub_category[0].list_img.length!=0)
-                    list_img_prodotto=categoriaProdotti[i].sub_category[0].list_img
-                else
-                    list_img_prodotto=[]
+        document.querySelector('.container_descrizione_prodotto h1').innerHTML = prodotto
+        document.querySelector('.container_descrizione_prodotto p').innerHTML = descrizione
+
+        while (document.querySelector('.circle_container_prodotti').firstChild) {
+            document.querySelector('.circle_container_prodotti').removeChild(document.querySelector('.circle_container_prodotti').firstChild);
+        }
+
+        if (list_sub_category.length > 1) {
+
+            for (let i = 0; i < list_sub_category.length; i++) {
+                let circle = document.createElement('div')
+                circle.className = 'circle'
+                if (i == index) circle.style.backgroundColor = '#D9D9D9';
+                document.querySelector('.circle_container_prodotti').appendChild(circle)
             }
         }
 
-        document.querySelector('.container_descrizione_prodotto h1').innerHTML=prodotto
-        document.querySelector('.container_descrizione_prodotto p').innerHTML=descrizione
-        document.querySelector('.container_prodotto').style.display='flex'
 
         if(list_img_prodotto.length!=0){
             document.querySelector('.immagine_prodotto').src= list_img_prodotto[0];
@@ -410,7 +438,6 @@
                 document.querySelector('.circle_container_img').removeChild(document.querySelector('.circle_container_img').firstChild);
             }
             for (let i=0;i<list_img_prodotto.length;i++){
-
                 let circle=document.createElement('div')
                 circle.className='circle'
                 if(i==0) circle.style.backgroundColor='#D9D9D9';
@@ -418,11 +445,18 @@
             }
 
             document.querySelector('.container_immagini').style.display='block';
-
         }else{
             document.querySelector('.container_immagini').style.display='none';
         }
 
+        document.querySelector('.container_prodotto').style.display='flex';
+
+    }
+
+    function changeImg(index){
+        document.querySelector('.immagine_prodotto').src=list_sub_category[index_sub_category].list_img[index]
+        document.querySelector('.container_immagini').style.backgroundImage="linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+list_sub_category[index_sub_category].list_img[index]+")"
+        document.querySelector('.circle_container_img').childNodes[index].style.backgroundColor="#D9D9D9"
     }
 
 /* ... */
@@ -445,18 +479,37 @@
         const deltaX = endX - startXslider;
         if (deltaX <-20) {
             document.querySelector('.circle_container_img').childNodes[index_current_img].style.backgroundColor="white"
-            if(index_current_img==list_img_prodotto.length-1)  index_current_img=0; else index_current_img=index_current_img+1;
-            document.querySelector('.immagine_prodotto').src=list_img_prodotto[index_current_img];
-            document.querySelector('.container_immagini').style.backgroundImage="linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+list_img_prodotto[index_current_img]+")"
-            console.log("Cambio colore circle")
-            document.querySelector('.circle_container_img').childNodes[index_current_img].style.backgroundColor="#D9D9D9"
+            if(index_current_img==list_sub_category[index_sub_category].list_img.length-1)  index_current_img=0; else index_current_img=index_current_img+1;
+            changeImg(index_current_img)
         }else{
             if (deltaX >20) {
                 document.querySelector('.circle_container_img').childNodes[index_current_img].style.backgroundColor="white"
-                if(index_current_img==0)  index_current_img=list_img_prodotto.length-1; else index_current_img=index_current_img-1;
-                document.querySelector('.immagine_prodotto').src=list_img_prodotto[index_current_img];
-                document.querySelector('.container_immagini').style.backgroundImage="linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("+list_img_prodotto[index_current_img]+")"
-                document.querySelector('.circle_container_img').childNodes[index_current_img].style.backgroundColor="#D9D9D9"
+                if(index_current_img==0)  index_current_img=list_sub_category[index_sub_category].list_img.length-1; else index_current_img=index_current_img-1;
+                changeImg(index_current_img)
+            }
+        }
+    })
+
+    container_descrizione_prodotto.addEventListener('touchstart', (e)=>{
+        startXslider=e.touches[0].clientX;
+    })
+
+    container_descrizione_prodotto.addEventListener('touchend', (e)=>{
+        const endX=e.changedTouches[0].clientX;
+        const deltaX = endX - startXslider;
+        if (deltaX <-20) {
+            if(list_sub_category.length>1) {
+                document.querySelector('.circle_container_prodotti').childNodes[index_sub_category].style.backgroundColor = "white"
+            }
+            if(index_sub_category==list_sub_category.length-1)  index_sub_category=0; else index_sub_category=index_sub_category+1;
+            showProdotto(index_sub_category)
+        }else{
+            if (deltaX >20) {
+                if(list_sub_category.length>1) {
+                    document.querySelector('.circle_container_prodotti').childNodes[index_sub_category].style.backgroundColor = "white"
+                }
+                if(index_sub_category==0)  index_sub_category=list_sub_category.length-1; else index_sub_category=index_sub_category-1;
+                showProdotto(index_sub_category)
             }
         }
     })
