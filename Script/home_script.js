80979cadcd7bb1a4b99f7da758c3bc5f686e85cc
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-11-30 13:02:48 +0100

    update

--- Script/home_script.js
+++ Script/home_script.js
@@ -27,7 +27,14 @@
         let copertina=document.querySelector('.copertina_main_img')
 
         if(window.scrollY!=0 && copertina.style.display!="none"){
-            window.scrollTo(0, 0);
+            //window.scrollTo(0, 0);
+            document.querySelector('.navbar').style.backgroundColor="rgba(255,255,255,0)";
+            let links=document.getElementsByClassName('link_navbar');
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].style.color != "rgb(240, 179, 87)") {
+                    links[i].style.color = "#000000";
+                }
+            }
 
             let scala=parseFloat(copertina.style.scale);
             scala=scala+0.1
@@ -35,8 +42,15 @@
             if(parseFloat(copertina.style.scale)>6){
                 copertina.style.opacity=0;
                 document.querySelector('.over-text').style.opacity=1;
+                for (let i = 0; i < links.length; i++) {
+                    if (links[i].style.color != "rgb(240, 179, 87)") {
+                        links[i].style.color = "#ffffff";
+                    }
+                }
             }
             if(parseFloat(copertina.style.scale)>8){
+                document.querySelector('.main_img').style.position="static";
+                window.scrollTo(0,0);
                 copertina.style.display="None"
                 animazioneCopertinaEseguita=true;
             }
@@ -69,6 +83,12 @@
 
     window.addEventListener('load',()=>{
         setSizeContainerShowRoom()
+        let links=document.getElementsByClassName('link_navbar');
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].style.color != "rgb(240, 179, 87)") {
+                links[i].style.color = "#000000";
+            }
+        }
     });
 
     window.addEventListener('scroll',()=>{
