12478d7f0ce5dbd4e3fa182830ce364bceee0e60
Angelo Afeltra <angelo.afeltra99@gmail.com>
2023-11-30 15:04:29 +0100

    update

--- Script/home_script.js
+++ Script/home_script.js
@@ -22,7 +22,7 @@
         }
     }
 
-
+    /*
     function animationCopertina() {
         let copertina=document.querySelector('.copertina_main_img')
 
@@ -54,6 +54,52 @@
                 copertina.style.display="None"
                 animazioneCopertinaEseguita=true;
             }
+        }
+    }
+
+   */
+
+    function animationCopertina() {
+        let copertina=document.querySelector('.copertina_main_img')
+
+        if(!animazioneCopertinaEseguita){
+            console.log(window.innerHeight)
+            //Identifico l'altezza dello scroll
+            console.log(window.scrollY)
+            window.scrollTo(0,0);
+
+
+
+
+
+            /*
+
+            document.querySelector('.navbar').style.backgroundColor="rgba(255,255,255,0)";
+            let links=document.getElementsByClassName('link_navbar');
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].style.color != "rgb(240, 179, 87)") {
+                    links[i].style.color = "#000000";
+                }
+            }
+
+            let scala=parseFloat(copertina.style.scale);
+            scala=scala+0.1
+            copertina.style.scale=scala.toString()
+            if(parseFloat(copertina.style.scale)>6){
+                copertina.style.opacity=0;
+                document.querySelector('.over-text').style.opacity=1;
+                for (let i = 0; i < links.length; i++) {
+                    if (links[i].style.color != "rgb(240, 179, 87)") {
+                        links[i].style.color = "#ffffff";
+                    }
+                }
+                window.scrollTo(0,0);
+            }
+            if(parseFloat(copertina.style.scale)>8){
+                document.querySelector('.main_img').style.position="static";
+                copertina.style.display="None"
+                animazioneCopertinaEseguita=true;
+            }*/
         }
     }
 
